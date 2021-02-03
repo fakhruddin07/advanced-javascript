2f4fa01e566bd5c4f6183b11caef09be12ddd6c3
fakhruddin07 <fakhruddinnoman95@gmail.com>
2021-02-03 09:05:23 +0600

    added common & interview questions

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,26 @@
+// No.1
+const first = 2;
+const second = '2';
+if(first == second){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
+
+
+// No.2
+const firstNum = 2;
+const secondNum = '2';
+if(firstNum === secondNum){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
+
+/*== : double equal jokhon use kora hoy tokhon value ta k r check kora hoy, kintu ki type er ba konta kon dhoroner ta r check kora hoy na. jar jonno 1 No. a number r string er majher difference ta check kore nai. tai tar result holo "true". ja shothik noy.
+=== : triple equal jokhon use kora hoy tokhon value & type ba konta kon dhoroner ta check kora hoy. jar jonno 2 No. a number r string er majher difference ta check kore. tai tar result holo "false". ja shothik*/
+
+/* #IMPORTANT & COMMON QUESTION FOR INTERVIEW #
+question : difference between double equal & triple equal?*/
