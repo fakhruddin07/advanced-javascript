c622d8cf8bb957f091a76fe33732b9231950922f
fakhruddin07 <fakhruddinnoman95@gmail.com>
2021-02-03 09:53:23 +0600

    added basic concept

--- map-filter.js
+++ map-filter.js
@@ -1,3 +1,4 @@
+// old method
 const numbers = [3, 5, 7, 9];
 const output = [];
 for(let i = 0; i < numbers.length; i++){
@@ -5,3 +6,38 @@ for(let i = 0; i < numbers.length; i++){
     const result = element * element;
     output.push(result);
 }
+
+console.log(output);
+
+
+// MAP :
+const evenNumbers = [2, 4, 6, 8, 10];
+const result = evenNumbers.map(function(element){
+    return element * element;
+})
+console.log(result);
+/*NOTE : map er vitore kono akti function k pass korle sha tinti parameter nite pare. (i)element; (ii)index; (iii)array;*/
+
+// shortcut system
+const total = evenNumbers.map( x => x * x );
+console.log(total);
+
+// FILTER :
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const bigger = number.filter(x => x > 5);
+
+console.log(bigger);
+
+
+// FIND :
+const naturalNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const findThat = naturalNumber.find(x => x > 5);
+
+console.log(findThat);
+
+
+/* map holo array er moddhe looping kore prottek ta element niye kisu akta kora & kora ses hoar pore prottek element niye jeta pabe sheta k result hishabe r akta array diye dibe.*/
+
+/*NOTE : 
+(i) map & filter array diye dibe.
+(ii) find akta element dibe.*/ 
