366f70bc33426c76cdcd9eefedaa153ce980a578
fakhruddin07 <fakhruddinnoman95@gmail.com>
2021-02-03 09:13:08 +0600

    added new code

---
+++ map-filter.js
@@ -0,0 +1,7 @@
+const numbers = [3, 5, 7, 9];
+const output = [];
+for(let i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
